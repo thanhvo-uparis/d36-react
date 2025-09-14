f3dce01d8bde39637e099bbd4e6d05dd9a540e8a
thanhvo <thanhvo.uparis@gmail.com>
2025-09-14 23:43:10 +0200

    display list of posts and add loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Loading.module.scss';
+
+function Loading() {
+    return (
+        <div className={styles.container}>
+            <h3>Loading...</h3>
+        </div>
+    )
+}
+
+export default Loading;
--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+import styles from './Posts.module.scss';
+import Loading from "../../components/Loading";
+
 function Posts() {
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(`https://jsonplaceholder.typicode.com/posts`)
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data);
+                setLoading(false);
+            })
+        }, 500);
+    }, [])
     return (
         <>
-        <p>Posts page content</p>
+        {loading && <Loading />}
+        <ul className={styles.postsContainer}>
+            {posts.map((item) => {
+                return (
+                    <li className={styles.itemContainer}>
+                        <div>
+                            <div className={styles.itemHeading}>
+                                <h3><span>User Id: </span>{item.userId}</h3>
+                            </div>
+                        </div>
+                        <div className={styles.itemContent}>
+                            <p>{item.title}</p>
+                            <p>{item.body}</p>
+                        </div>
+                        <div className={styles.itemFooter}>
+                            <button className={styles.itemBtn}>Details</button>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
         </>
     )
 }
