a0d2757213662bf5a1cfa488724ea114e7238a51
thanhvo <thanhvo.uparis@gmail.com>
2025-09-15 03:40:54 +0200

    chuyển trang ở pagination và thêm param page trên url

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import styles from './Posts.module.scss';
 import Loading from "../../components/Loading";
 
@@ -7,6 +7,8 @@ function Posts() {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [totalPosts, setTotalPosts] = useState(0);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [page, setPage] = useState(+searchParams.get("page") || 1);
 
     const getSizePost = async () => {
         const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -20,17 +22,27 @@ function Posts() {
 
     useEffect(() => {
         setTimeout(() => {
-            fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`)
+            fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`)
             .then(res => res.json())
             .then(data => {
                 setPosts(data);
                 setLoading(false);
             })
         }, 500);
-    }, [])
+    }, [page])
+
+    useEffect(() => {
+        setSearchParams({page: page})
+    }, [page])
+
+    const handleSelectedPage = (index) => {
+        setPage(index + 1);
+    }
+
     return (
         <>
         {loading && <Loading />}
+        {/* Pagination */}
         {
             loading ? "" :
             <div className={styles.pagination}>
@@ -39,20 +51,21 @@ function Posts() {
                     {Array(Math.floor(totalPosts / 10)).fill().map((_, index) => 
                     {
                         return (
-                            <li key={index}><Link className={styles.itemLink}>{index + 1}</Link></li>
+                            <li key={index} className={(page === (index + 1)) ? styles.active : ""}><Link onClick={() => handleSelectedPage(index)} className={styles.itemLink}>{index + 1}</Link></li>
                         )
                     })}
                     <li><i class="fa-solid fa-right-long"></i></li>
                 </ul>
             </div>
         }
+        {/* render item (post) */}
         <ul className={styles.postsContainer}>
             {posts.map((item) => {
                 return (
-                    <li className={styles.itemContainer}>
+                    <li className={styles.itemContainer} data-id={item.id}>
                         <div>
                             <div className={styles.itemHeading}>
-                                <h3><span>User Id: </span>{item.userId}</h3>
+                                <h3><span>Id: </span>{item.id}</h3>
                             </div>
                         </div>
                         <div className={styles.itemContent}>
@@ -60,7 +73,7 @@ function Posts() {
                             <p>{item.body}</p>
                         </div>
                         <div className={styles.itemFooter}>
-                            <button className={styles.itemBtn}>Details</button>
+                            <button onClick={() => {}} className={styles.itemBtn}>Details</button>
                         </div>
                     </li>
                 )
