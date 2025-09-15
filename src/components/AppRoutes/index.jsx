9566cd4845ef40544aa26e9328a69ae252f1b703
thanhvo <thanhvo.uparis@gmail.com>
2025-09-15 15:33:33 +0200

    add contact and privacy pages

--- src/components/AppRoutes/index.jsx
+++ src/components/AppRoutes/index.jsx
@@ -2,8 +2,14 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
 import Posts from "../../pages/Posts";
+import PostDetail from "../../pages/PostDetail";
 import Header from "../../layouts/components/Header";
 import DefaultLayout from "../../layouts/DefaultLayout";
+import AuthLayout from "../../layouts/AuthLayout";
+import Login from "../../pages/Login";
+import Register from "../../pages/Register";
+import Contact from "../../pages/Contact";
+import Privacy from "../../pages/Privacy";
 
 function AppRoutes() {
     return (
@@ -13,6 +19,13 @@ function AppRoutes() {
                     <Route index element={<Home />}/>
                     <Route path="/about" element={<About />}/>
                     <Route path="/posts" element={ <Posts /> }/>
+                    <Route path="/post/:id" element={ <PostDetail /> }/>
+                    <Route path="/contact" element={<Contact />}/>
+                    <Route path="/privacy" element={<Privacy />}/>
+                </Route>
+                <Route element={<AuthLayout />}>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/register" element={<Register />}/>
                 </Route>
             </Routes>
         </BrowserRouter>
